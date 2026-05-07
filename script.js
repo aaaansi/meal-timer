0f8d7d18d8c2418a1992296ac691292bb4879cd4
Hamzah Alansi <theshortrolow@gmail.com>
2026-05-08 10:01:08 +1200

    exercise time handling + basic css styling

--- script.js
+++ script.js
@@ -5,19 +5,84 @@ function calculate() {
     const inputElementWakeUpTime = document.getElementById("wakeupTime").value;
     const inputElementSleepTime = document.getElementById("sleepTime").value;
     const inputElementExerciseTime = document.getElementById("exerciseTime").value;
+    const results = document.getElementById("results");
+
+    const validWakeUpTime = validateInputs(inputElementWakeUpTime);
+    const validSleepTime = validateInputs(inputElementSleepTime);
+    const validExerciseTime = validateInputs(inputElementExerciseTime);
+
+    // if (!validWakeUpTime || !validSleepTime){
+    //     results.innerHTML = "<p>Please Enter Wake & Sleep times!<p>"
+    //     return;
+    // }
+    // if (timeToMinutes(inputElementWakeUpTime) > timeToMinutes(inputElementSleepTime)){
+    //     results.innerHTML = "<p>Do you wake up before you sleep?<p>"
+    //     return;
+    // }
+    
     
     const bestTimeToEatBreakfast = minutesToTime(timeToMinutes(inputElementWakeUpTime) + 30);
     const bestTimeToEatSnack = minutesToTime(timeToMinutes(inputElementWakeUpTime) + HOURS * 3);
     const bestTimeToEatLunch = minutesToTime(timeToMinutes(inputElementWakeUpTime) + HOURS * 5);
     const bestTimeToEatDinner = minutesToTime(timeToMinutes(inputElementSleepTime) - HOURS * 3);
     const bestTimeToStopEating = minutesToTime(timeToMinutes(inputElementSleepTime) - HOURS * 2);
+    results.innerHTML = ``;
+    results.innerHTML += `<div class="meal-card">
+        <span class="meal-label">🌅 Breakfast: </span>
+        <span class="meal-time">${bestTimeToEatBreakfast}</span>
+        <span class="meal-type">Heavy meal</span>
+        </div>`;
+    results.innerHTML +=  `<div class="meal-card">
+        <span class="meal-label">🍿 Snack time: </span>
+        <span class="meal-time">${bestTimeToEatSnack}</span>
+        <span class="meal-type">Light meal</span>
+        </div>`;
+    results.innerHTML +=  `<div class="meal-card">
+        <span class="meal-label">☀️ Lunch: </span>
+        <span class="meal-time">${bestTimeToEatLunch}</span>
+        <span class="meal-type">Moderate meal</span>
+        </div>`;
+    results.innerHTML +=  `<div class="meal-card">
+        <span class="meal-label">🌄 Dinner: </span>
+        <span class="meal-time">${bestTimeToEatDinner}</span>
+        <span class="meal-type">Light meal</span>
+        </div>`;
+        results.innerHTML +=  `<div class="meal-card">
+        <span class="meal-label">🎑 Stop Eating: </span>
+        <span class="meal-time">${bestTimeToStopEating}</span>
+        <span class="meal-type"></span>
+        </div>`;
+    
+    if (validExerciseTime){
+        const bestTimetoEatpreWorkoutSnack = minutesToTime(timeToMinutes(inputElementExerciseTime) - HOURS);
+        const bestTimetoEatpreWorkoutMeal = minutesToTime(timeToMinutes(inputElementExerciseTime) - HOURS * 2.5);
+        const bestTimetoEatpostWorkout = minutesToTime(timeToMinutes(inputElementExerciseTime) + HOURS / 2);
+        const bestTimetoEatpreSleep = minutesToTime(timeToMinutes(inputElementSleepTime) - HOURS / 2);
+        results.innerHTML += `<div class="meal-card">
+            <span class="meal-label">⚡ Preworkout snack: </span>
+            <span class="meal-time">${bestTimetoEatpreWorkoutSnack}</span>
+            <span class="meal-type">Light meal</span>
+            </div>`;
+        results.innerHTML +=  `<div class="meal-card">
+            <span class="meal-label">🍽️ Preworkout meal: </span>
+            <span class="meal-time">${bestTimetoEatpreWorkoutMeal}</span>
+            <span class="meal-type">Heavy meal</span>
+            </div>`;
+        results.innerHTML +=  `<div class="meal-card">
+            <span class="meal-label">💪 Post workout: </span>
+            <span class="meal-time">${bestTimetoEatpostWorkout}</span>
+            <span class="meal-type">Recovery meal</span>
+            </div>`;
+        results.innerHTML +=  `<div class="meal-card">
+            <span class="meal-label">🌙 Pre sleep: </span>
+            <span class="meal-time">${bestTimetoEatpreSleep}</span>
+            <span class="meal-type">Casein protein</span>
+            </div>`;
+    }
+}
 
-    console.log("Best time to eat breakfast is: " + bestTimeToEatBreakfast);
-    console.log("Best time to eat a snack is: " + bestTimeToEatSnack);
-    console.log("Best time to eat Lunch is: " + bestTimeToEatLunch);
-    console.log("Best time to eat Dinner is: " + bestTimeToEatDinner);
-    console.log("Best time to stop eating is: " + bestTimeToStopEating);
-
+function validateInputs(timeInput){
+    return timeInput !== "";
 }
 
 function timeToMinutes(timeString){
